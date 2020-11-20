ab861d35131fb0458d06b73cf69ea8be849d9f88
buba1301 <bmuza57@gmail.com>
2020-11-20 15:26:00 +0300

    After first three lessons

--- main.js
+++ main.js
@@ -1,70 +1,88 @@
 const $buttons = document.querySelectorAll('.button');
 
-const getRandomNum = (num) => {
-	return Math.ceil(Math.random() * num);
+function getRandomNum(dmgLevel) {
+	return Math.ceil(Math.random() * this.lifePoitns) * dmgLevel;
 };
+
+function renderHpLife() {
+	this.elHp.innerHTML = [this.damageHP, this.defaultHP].join(' / ');
+};
+
+function renderProgressbar() {
+	this.elProgressbar.style.width = this.damageHP + '%';
+};
+
+function renderHP() {
+	this.renderHpLife();
+	this.renderProgressbar();
+};
+
+function buttonsDisabled() {
+	$buttons.forEach(($button) => $button.disabled = true);
+};
+
+function changeHP(count) {
+	if (this.damageHP < count) {
+		this.damageHP = 0;
+		alert(this.name + ' is died!!');
+		buttonsDisabled();
+	} else {
+		this.damageHP -= count;
+	}
+	this.renderHpLife();
+	this.renderProgressbar();
+};
+
+const handleClick = (e) => {
+	const $buttonId = e.target.id;
+	const dmgLevel = Number($buttonId.slice(-1));
+
+	const count = damageLevel[$buttonId](dmgLevel);
+	character.changeHP(count);
+	enemy.changeHP(count);
+};
+
+$buttons.forEach(($button) => $button.addEventListener('click', handleClick));
 
 const character = {
 	name: 'Pikachu',
 	defaultHP: 100,
 	damageHP: 100,
+
 	elHp: document.getElementById('health-character'),
 	elProgressbar: document.getElementById('progressbar-character'),
+
+	renderHpLife: renderHpLife,
+	renderProgressbar: renderProgressbar,
+	renderHP: renderHP,
+	changeHP: changeHP,
 };
 
 const enemy = {
 	name: 'Charmander',
 	defaultHP: 100,
 	damageHP: 100,
+
 	elHp: document.getElementById('health-enemy'),
 	elProgressbar: document.getElementById('progressbar-enemy'),
+
+	renderHpLife: renderHpLife,
+	renderProgressbar: renderProgressbar,
+	renderHP: renderHP,
+	changeHP: changeHP,
 };
 
 const damageLevel = {
-	'btn-kick-1': () => getRandomNum(20),
-	'btn-kick-2': () => getRandomNum(20) * 2,
+	lifePoitns: 20,
+	damageLevel1: 1,
+	damageLevel2: 2,
+	'btn-kick-1': getRandomNum,
+	'btn-kick-2': getRandomNum,
 }
-
-const renderHpLife = (person) => {
-	person.elHp.innerHTML = [person.damageHP, person.defaultHP].join(' / ');
-};
-
-const renderProgressbar = (person) => {
-	person.elProgressbar.style.width = person.damageHP + '%';
-};
-
-const renderHP = (person) => {
-	renderHpLife(person);
-	renderProgressbar(person);
-}
-
-const buttonsDisabled = () => {
-	$buttons.forEach(($button) => $button.disabled = true);
-}
-
-const changeHP = (count, person, $button) => {
-	if (person.damageHP < count) {
-		person.damageHP = 0;
-		alert(person.name + ' is died!!');
-		buttonsDisabled();
-	} else {
-		person.damageHP -= count;
-	}
-	renderHpLife(person);
-	renderProgressbar(person);
-};
-
-const handleClick = (e) => {
-	const $buttonId = e.target.id;
-	changeHP(damageLevel[$buttonId](), character);
-	changeHP(damageLevel[$buttonId](), enemy);
-};
-
-$buttons.forEach(($button) => $button.addEventListener('click', handleClick));
 
 const init = () => {
-	renderHP(character);
-	renderHP(enemy)
+	character.renderHP();
+	enemy.renderHP();
 };
 
-init()
+init();
